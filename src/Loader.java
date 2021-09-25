935bca7d64d6b63103ae48517240747f9fb1c92c
Erick Capilla <erickpoblano25@gmail.com>
2021-09-24 20:16:55 -0500

    Loader done

--- src/Loader.java
+++ src/Loader.java
@@ -20,55 +20,75 @@ public class Loader {
   }
 
   public void reMemory(String position) {
-    ArrayList<String> hexa = new ArrayList<>(Arrays.asList(hex));
+    ArrayList<String> hexa = new ArrayList<>(Arrays.asList(this.hex));
     int index1 = hexa.indexOf(String.valueOf(position.toUpperCase().charAt(0)));
     int index2 = hexa.indexOf(String.valueOf(position.toUpperCase().charAt(1)));
 
     for(int i = 0; i < this.memory.size(); i++) {
-      this.newMemory.add(this.memory.get(i));
-      this.newMemory.get(i)[0] = String.valueOf(position);
+      String[] mem = { "", this.memory.get(i)[1] };
+      this.newMemory.add(mem);
+      this.newMemory.get(i)[0] = String.valueOf(position).toUpperCase();
       index2++;
-      position = generateMemory(index1, index2);
-      if(index2 >= 15) {index1++; index2 = 0;}
-      if(index1 >= 15) index1 = 0;
+      if(index1 >= 15 && index2 > 15) {
+        this.jumps();
+        this.newMemory.forEach(m -> System.out.println(Arrays.toString(m)));
 
-      if(this.memory.get(i)[2].contains("*")) {
-        i++;
-        String content = this.memory.get(i)[1];
-        addHex(content);
+        if(this.memory.size() == this.newMemory.size()) writeFile();
+         else System.out.println("Memoria Insuficiente");
+
+        System.exit(0);
       }
+      if(index2 > 15) {index1++; index2 = 0;}
+      if(index1 > 15) index1 = 0;
+      position = generateMemory(index1, index2);
     }
 
+    this.jumps();
     writeFile();
   }
 
   public String generateMemory(int index1, int index2) {
-    String num = hex[index1]+hex[index2];
+    String num = this.hex[index1] + this.hex[index2];
     return num;
   }
 
-  public void addHex(String content) {
+  public void jumps() {
+    for(int i = 0; i < this.memory.size(); i++) {
+      if(this.memory.get(i)[2].contains("*")) {
+        String content = this.memory.get(i+1)[1];
+        this.newMemory.get(i+1)[1] = addHex(content);
+      }
+    }
+  }
+
+  public String addHex(String content) {
     ArrayList<String> hexa = new ArrayList<>(Arrays.asList(hex));
     int aux = hexa.indexOf(String.valueOf(this.positionFree.toUpperCase().charAt(0)));
     int aux2 = hexa.indexOf(String.valueOf(this.positionFree.toUpperCase().charAt(1)));
     int first = hexa.indexOf(String.valueOf(content.toUpperCase().charAt(0)));
     int second = hexa.indexOf(String.valueOf(content.toUpperCase().charAt(1)));
-
+    String newHex = "";
 
     for (int i = 0; i < aux; i++) {
-
+      first++;
     }
-
     for (int i = 0; i < aux2; i++) {
-
+      second++;
+      if(second >= 16) {first++; second = 0;}
     }
+    if(first > 16)
+      System.out.println("Memoria insuficiente");
+    else
+    newHex = hexa.get(first)+hexa.get(second);
+
+    return newHex;
   }
 
   public void writeFile() {
     try {
       FileWriter file = new FileWriter(this.fileOut + ".rlz");
       PrintWriter writer = new PrintWriter(file);
-      writer.println("[Memoria, Codigos, Mod]");
+      writer.println("[Memoria, Codigos]");
 
       this.newMemory.forEach(m -> writer.println(Arrays.toString(m)));
 
