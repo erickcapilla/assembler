cb26bdfb5ceebca92419d28907de8fd72ba14de5
Erick Capilla <erickpoblano25@gmail.com>
2021-09-24 22:18:31 -0500

    Busy Memory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,30 +6,68 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     ArrayList<String[]> tokens;
+    ArrayList<String> busyMemory = new ArrayList<>();
+    String op;
 
-    System.out.print("Ruta/archivo: ");
-    String fileIn = sc.nextLine();
+    do {
+      System.out.print("Ruta/archivo: ");
+      String fileIn = sc.nextLine();
 
-    System.out.print("Nombre del archivo/salida (sin extencion): ");
-    String fileOut = sc.nextLine();
+      System.out.print("Nombre del archivo/salida (sin extencion): ");
+      String fileOut = sc.nextLine();
 
-    Tokens token = new Tokens(fileIn, fileOut);
+      Tokens token = new Tokens(fileIn, fileOut);
 
-    tokens = token.getTokens();
+      tokens = token.getTokens();
 
-    Tables tables = new Tables(tokens, fileIn,fileOut);
+      Tables tables = new Tables(tokens, fileIn,fileOut);
 
-    System.out.print("Ejecutar Cargador [Y/N]: ");
-    String option = sc.nextLine();
+      System.out.print("Ejecutar Cargador [Y/N]: ");
+      String option = sc.nextLine();
 
-    if(option.equalsIgnoreCase("y")) {
-      System.out.print("Memoria libre: ");
-      String position = sc.nextLine();
-      if(position.length() < 2) position = "0" + position;
-      Loader loader = new Loader(tables.getMemoryPositions(), position, fileOut);
-    } else {
-      System.exit(0);
+      boolean load = false;
+      do {
+        boolean exe = true;
+        if(option.equalsIgnoreCase("y")) {
+          System.out.print("Memoria libre: ");
+          String position = sc.nextLine();
+          if(position.length() < 2) position = "0" + position;
+          if(noAvailable(position, busyMemory)) {
+            System.out.println("Memoria ocupada por el SO, Memoria disponible a partir de: " +
+                    busyMemory.get(busyMemory.size() - 1));
+            exe = false;
+            load = true;
+          }
+
+          if(exe) {
+            Loader loader = new Loader(tables.getMemoryPositions(), position, fileOut);
+          }
+        }
+      } while (load);
+
+      System.out.print("¿Volver a ejecutar programa? [Y/N]: ");
+      op = sc.nextLine();
+    } while (op.equalsIgnoreCase("y"));
+
+  }
+
+  public static boolean noAvailable(String position, ArrayList<String> busyMemory) {
+    final String[] hex = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F"};
+
+    int i = 0, j = 0, count = 0;
+    boolean res = false;
+
+    for(int k = 0; k < 20; k++) {
+      busyMemory.add(hex[i]+hex[j]);
+      if(position.equalsIgnoreCase(hex[i]+hex[j])) count++;
+      j++;
+      if(j > 15) {i++; j = 0;}
     }
+    busyMemory.add(hex[i]+hex[j]);
 
+    if(count != 0) res = true;
+
+    return res;
   }
 }
